709b462f130c7ae9fd9b0c3cd25d1f7487ee936f
Aravind <aravind4113546@gmail.com>
2016-11-15 23:54:07 -0500

    Changing example function

--- example_timer.c
+++ example_timer.c
@@ -5,18 +5,17 @@ long int count = 0;
 void timer1_func(void)
 {
   count++;
-}                                                               
+}
 
 void timer2_func(void)
 {
   printf("%ld\n",count);
   count = 0;
-}                   
-                  
+}
 
 int main()
 {
-  start_timer(timer1_func, 100000, 1);		
+  start_timer(timer1_func, 500000, 1);
   start_timer(timer2_func, 1, 1);
   while(1);
 }
